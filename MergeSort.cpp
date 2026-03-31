b02bc413ab43d0789e8960a13faf4adc687a988d
taufiq72 <taufiqq1207@gmail.com>
2026-03-31 11:43:20 +0700

    step 4a

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -59,5 +59,20 @@ void mergeSort(int low, int high)
             B[k] = arr[j];
             j++;
         }
+        k++;
+    }
+
+    while (j <= high)
+    {
+        B[k] = arr[j];
+        j++;
+        k = k + 1;
+    }
+
+    while (i <= mid)
+    {
+        B[k] =arr[i];
+        i++;
+        k++;
     }
 }
