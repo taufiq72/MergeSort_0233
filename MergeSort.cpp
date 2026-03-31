4f83ebc5661ee77bd09789d3abda98f988e80fe2
taufiq72 <taufiqq1207@gmail.com>
2026-03-31 11:35:48 +0700

    menentukan low high and mid

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -33,5 +33,13 @@ void input()
 
 void mergeSort(int low, int high)
 {
-    
+    if (low >= high)
+    {
+        return;
+    }
+
+    int mid = (low + high) / 2;
+
+    mergeSort(low, mid);
+    mergeSort(mid + 1, high);
 }
