d79c783a65ffc6bb5273067555a410f1cc575b1f
taufiq72 <taufiqq1207@gmail.com>
2026-03-31 11:28:58 +0700

    input array

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -6,5 +6,18 @@ int n;
 
 void input()
 {
-    
+    while (true)
+    {
+        cout << "Masukan Panjang element array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+    }
 }
