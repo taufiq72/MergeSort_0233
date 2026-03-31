ddb5d45064941615a7cd57746a93d5c37c5420b4
taufiq72 <taufiqq1207@gmail.com>
2026-03-31 11:32:21 +0700

    for di input

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -20,4 +20,13 @@ void input()
             cout << "\nMaksimal panjang array adalah 20";
         }
     }
+    cout << "\n--------------------------";
+    cout << "\nInputkan Isi element array";
+    cout << "\n--------------------------";
+
+    for (int i=0; i < n; i++)
+    {
+        cout << "Array index ke- " << i << " : ";
+        cin >> arr[i];
+    }
 }
